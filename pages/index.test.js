80209dd98f7c4b13ea21a7d7e061d204cc16e9e4
vinaekalaylani <vinaekalaylani@gmail.com>
2021-11-25 11:50:12 +0700

    first-commit

--- pages/index.test.js
+++ pages/index.test.js
@@ -5,11 +5,7 @@ enzyme.configure({ adapter: new Adapter() });
 
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Sidebar from '../components/sidebar'
 
 it('renders correctly', () => {
-  const tree = renderer
-    .create(<Sidebar />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  
 });
